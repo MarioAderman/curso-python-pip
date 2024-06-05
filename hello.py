ba6b29b804fa242be844c2b73a9cea0e9da4a2a2
root <root@LAPTOP-J51BTH8I>
2024-06-04 22:17:04 -0600

    Mi primer archivo

---
+++ hello.py
@@ -0,0 +1,12 @@
+print('Hola desde mi maquina')
+primos = [2, 3, 5, 7, 11, 13, 17, 19]
+print(primos[3])
+print(True and False)
+name = 'Mario Salvador'
+name = name.replace('Mario', 'Juan')
+print(name)
+a = {1,2}
+b = {2,3}
+print(a & b)
+names = {'Nicolas', 'Miguel', 'Juan', 'Nicolas'} 
+print(names) 
